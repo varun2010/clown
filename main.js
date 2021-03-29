9a063d4c21851f6b5abfe0a4d6bfdfedda48debd
varun2010 <76247848+varun2010@users.noreply.github.com>
2021-03-29 20:40:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var noseX=0;
+var noseY=0;
+function preload(){
+    clown=loadImage("https://i.postimg.cc/c4h751xh/580b57fbd9996e24bc43bed5.png");
+}
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(640,480);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("poseNet is Initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x-15;
+        noseY=results[0].pose.nose.y-15;
+        console.log("nose x = "+results[0].pose.nose.x);
+        console.log("nose y = "+results[0].pose.nose.y);
+    }
+}
+function draw(){
+    image(video,0,0,640,480);
+    image(clown,noseX,noseY,40,40);
+}
+function take_snapshot(){
+    save("selfie.png");
+}
